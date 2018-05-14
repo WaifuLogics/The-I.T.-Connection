bbe22d97ae8fc798b6f21d3dc08334c10c1f3fbe
Duncan Sterken <duncan.sterken123@gmail.com>
2018-05-14 09:50:15 +0200

    aaaa

--- app/router.php
+++ app/router.php
@@ -6,6 +6,7 @@ $app->get('/', function (Request $request, Response $response, $args) {
     return $this->view->render($response, 'home.twig', [
         //an array of variables that can be used in twig
         'color' => 'Jemoeder',
-        'name' => 'Guylian'
+        'name' => 'Guylian',
+        'items' => array_merge(range('A', 'Z'), range('a', 'z'))
     ]);
 })->setName('home');
