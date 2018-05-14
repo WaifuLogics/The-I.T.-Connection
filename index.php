52bc0ea54c610fd4cbdf9c307bcfbe2f10671944
Guylian Gilsing <78902@rocteraa-student.nl>
2018-05-14 09:06:15 +0200

    Added basic folder structure + Frontend Frameworks

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>The I.T. Connection</title>
+    <!--Materialize-->
+    <link href="framework/materialize/css/materialize.min.css" rel="stylesheet" type="text/css">
+    <!--Animate.css-->
+    <link href="framework/animate.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <!--Materialize-->
+    <script src="framework/materialize/js/materialize.min.js" type="text/javascript"></script>
+  </body>
+</html>
