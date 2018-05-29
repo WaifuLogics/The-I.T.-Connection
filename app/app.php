4ad73d93198c53ace17a61f6b7b63cffbe560c67
Guylian Gilsing <78902@rocteraa-student.nl>
2018-05-29 13:41:28 +0200

    Fixes for register page and php error

--- app/app.php
+++ app/app.php
@@ -28,7 +28,7 @@ $container['view'] = function ($c) {
     ));
 
     $view->offsetSet('loggedIn', !empty($_SESSION['user_name']));
-    $view->offsetSet("accountName", $_SESSION['user_name']);
+    $view->offsetSet("accountName", !empty($_SESSION['user_name']));
 
     return $view;
 };
