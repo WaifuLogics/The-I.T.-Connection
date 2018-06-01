49df0055371519bc23ad2d69e55965b1ab529ff0
Guylian Gilsing <78902@rocteraa-student.nl>
2018-06-01 16:40:47 +0200

    Added Non Functional Search Backend Code

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,7 @@
 <?php
 $GLOBALS['config'] = [
-
+    'servername' => "protask.duncte123.me",
+    'username' => "pro",
+    'password' => "30Fos5L1Y",
+    'dbname' => "it_connection"
 ];
--- js/global.js
+++ js/global.js
@@ -5,3 +5,19 @@ function SetActive(mode) {
         i.className += " active";
     }
 }
+
+function Search(f_username) {
+    /*The headers used to post data*/
+    let headers = {
+        method: 'post',
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        body: 'x=8==D'
+    };
+
+    fetch('/inc/searchScript.php', headerInformation).then(response => response.text())
+        .then(data => {
+            document.getElementById('container-search_accounts_output').innerHTML += data;
+        });
+}
