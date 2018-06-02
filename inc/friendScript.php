27cfd50625ed4daaca8fa863e58b4ee0131bb711
Guylian Gilsing (Desktop) <78902@rocteraa-student.nl>
2018-06-02 15:32:20 +0200

    Send Requests Work

---
+++ inc/friendScript.php
@@ -0,0 +1,29 @@
+<?php
+include('../app/app.php');
+/* Assign the PDO object to a local variable */
+
+if(isset($_POST['type']) && $_POST['type'] != ""){
+  switch ($_POST['type']){
+    case "request":
+      /* Get's activated when someone sends a friend request */
+      $curUser = $_POST['curUser'];
+      $othUser = $_POST['othUser'];
+
+      $stmnt = $container->database->prepare("INSERT INTO friend_request (current_user_id, other_user_id, request_time)
+                       VALUES (:curUs, :othUs, :reqTim);");
+      $stmnt->execute([
+        'curUs' => $curUser,
+        'othUs' => $othUser,
+        'reqTim' => date("y-m-d H:i:s"),
+      ]);
+      var_dump($stmnt->errorInfo());
+      break;
+    case "accept":
+      /* Get's activated when someone accepts a friend request */
+      break;
+    default:
+      echo "<p>"."Failed to resolve request"."</p>";
+  }
+}else{
+  echo "<p>"."Access Denied"."</p>";
+}
--- js/global.js
+++ js/global.js
@@ -1,4 +1,5 @@
 let _ = (el) => document.getElementsByClassName(el);
+let GetId = (el) => document.getElementById(el);
 
 function SetActive(mode) {
     for (let i of _(mode)) {
@@ -6,9 +7,9 @@ function SetActive(mode) {
     }
 }
 
-function Search(f_username) {
+function Search(f_username, thisUser) {
 
-  let bodyInfo = 'search-username=' + f_username;
+  let bodyInfo = 'search-username=' + f_username + "&search_thisUser=" + thisUser;
 
     /*The headers used to post data*/
     let headers = {
@@ -23,4 +24,30 @@ function Search(f_username) {
         .then(data => {
             document.getElementById('container-search_accounts_output').innerHTML = data;
         });
+}
+
+function AddFriendButton(){
+  AddFriend(GetId('myId').value, GetId('search_user_id').value);
+}
+
+function AddFriend(cur_user, oth_user) {
+  let currentUser = cur_user;
+  let otherUser = oth_user;
+
+
+  let bodyInfo = 'type=request&curUser=' + currentUser + '&othUser=' + otherUser;
+  let headers = {
+    method:'post',
+    headers: {
+      "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+    },
+    body: bodyInfo
+  }
+  /* Insert a friend request in the friend_request table*/
+  fetch('/inc/friendScript.php', headers).then(response => response.text())
+  .then(data => {
+    if(data == "success"){
+      
+    }
+  });
 }
