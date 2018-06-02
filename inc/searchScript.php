49429ab50d529f14b328c3e922e7238f5a295444
Guylian Gilsing (Desktop) <78902@rocteraa-student.nl>
2018-06-02 13:32:44 +0200

    Functional Search Page + Script

--- inc/searchScript.php
+++ inc/searchScript.php
@@ -1,1 +1,27 @@
 <?php
+  /* Include the app file to get acces to the PDO object */
+  include('../app/app.php');
+
+  /* Check if the POST variable is set and isn't empty */
+  if(isset($_POST['search-username']) && $_POST['search-username'] != ""){
+    /* Create a variable that acts as the search arguments*/
+    $searchName = "%".$_POST['search-username']."%";
+
+    /* Search for the $searchName contents in the database */
+    $stmnt = $container->database->prepare("SELECT account_id, account_name FROM accounts WHERE account_name LIKE :searchName");
+    $stmnt->execute([':searchName' => $searchName]);
+
+    $result = $stmnt->fetchAll(PDO::FETCH_ASSOC);
+    if(count($result) > 0){
+      foreach ($result as $data) {
+        /* Display all of the found accounts */
+        echo "<div>";
+          echo "<p>".$data['account_name']."</p>";
+        echo "</div>";
+      }
+    }else{
+      echo "<p>"."No accounts found."."</p>";
+    }
+  }else{
+    echo "<p>Please enter the name of the account you want to search for</p>";
+  }
--- js/global.js
+++ js/global.js
@@ -7,17 +7,20 @@ function SetActive(mode) {
 }
 
 function Search(f_username) {
+
+  let bodyInfo = 'search-username=' + f_username;
+
     /*The headers used to post data*/
     let headers = {
         method: 'post',
         headers: {
             "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
         },
-        body: 'x=8==D'
+        body: bodyInfo
     };
 
-    fetch('/inc/searchScript.php', headerInformation).then(response => response.text())
+    fetch('/inc/searchScript.php', headers).then(response => response.text())
         .then(data => {
-            document.getElementById('container-search_accounts_output').innerHTML += data;
+            document.getElementById('container-search_accounts_output').innerHTML = data;
         });
 }
